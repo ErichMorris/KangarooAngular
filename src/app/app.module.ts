724b184cc4de12fac8029a21fabe60e34c3a108a
pooja197 <47895436+pooja197@users.noreply.github.com>
2019-05-06 12:59:45 -0400

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import {
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/header/header.component'
 import { RegistrationComponent } from './components/registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -31,27 +31,22 @@ import { RestaurantDetailComponent } from './components/restaurant/restaurant-de
 import { RestaurantEditComponent } from './components/restaurant/restaurant-edit/restaurant-edit.component';
 import { RestaurantDeleteComponent } from './components/restaurant/restaurant-delete/restaurant-delete.component';
 
-
-
 import { CustomerIndexComponent } from './components/customer/customer-index/customer-index.component';
 import { CustomerCreateComponent } from './components/customer/customer-create/customer-create.component';
 import { CustomerDetailComponent } from './components/customer/customer-detail/customer-detail.component';
 import { CustomerEditComponent } from './components/customer/customer-edit/customer-edit.component';
 import { CustomerDeleteComponent } from './components/customer/customer-delete/customer-delete.component';
 
-
 import { OrderCreateComponent } from './components/order/order-create/order-create.component';
 import { OrderDetailComponent } from './components/order/order-detail/order-detail.component';
 import { OrderEditComponent } from './components/order/order-edit/order-edit.component';
 import { OrderDeleteComponent } from './components/order/order-delete/order-delete.component';
-
 
 import { MenuItemIndexComponent } from './components/menuItem/menu-item-index/menu-item-index.component';
 import { MenuItemCreateComponent } from './components/menuItem/menu-item-create/menu-item-create.component';
 import { MenuItemDetailComponent } from './components/menuItem/menu-item-detail/menu-item-detail.component';
 import { MenuItemEditComponent } from './components/menuItem/menu-item-edit/menu-item-edit.component';
 import { MenuItemDeleteComponent } from './components/menuItem/menu-item-delete/menu-item-delete.component';
-
 
 const routes =
   [
@@ -99,7 +94,7 @@ const routes =
     RestaurantDeleteComponent,
     LoginComponent,
     RestaurantCreateComponent,
-    OrderIndexComponent,
+    //OrderIndexComponent,
     CustomerIndexComponent,
     CustomerCreateComponent,
     CustomerDetailComponent,
@@ -119,6 +114,7 @@ const routes =
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    RouterModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
     ReactiveFormsModule,
