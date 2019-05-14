0588f8158304872d6a92e9239af7b8e27e1981c6
Erich Morris <emorris1692@gmail.com>
2019-05-14 16:34:59 -0400

    crud works!

--- src/app/components/user-restaurant/user-restaurant.component.ts
+++ src/app/components/user-restaurant/user-restaurant.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { RestaurantsService } from 'src/app/services/restaurants.service';
+import { Restaurant } from 'src/app/models/Restaurant';
 
 @Component({
   selector: 'app-user-restaurant',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRestaurantComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _userRestaurantService: RestaurantsService) { }
 
   ngOnInit() {
+    this._userRestaurantService.getRestaurants().subscribe((restaurants: Restaurant[]) => {
+      this.dataSource = new MatTableDataSource<Restaurant>(restaurants); 
+    });
   }
-
+  columnNames = ['Name', 'Address', 'ContactNumber', 'Rating', 'buttons'];
+  dataSource: MatTableDataSource<Restaurant>;
 }
